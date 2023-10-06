5cb69312bab3c9a3898b2af2b0d6416eb7b622f6
alamin <malaminmiu@gmail.com>
2023-10-06 11:01:07 +0600

    update

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,152 @@
-# blog/views.py
-
+from rest_framework import permissions
 from rest_framework import generics
+from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Post, Comment
-
+import jwt, datetime
+from rest_framework.authentication import TokenAuthentication
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.exceptions import AuthenticationFailed
+
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    def get(self, request):
+        authorization_header = request.headers.get('Authorization')
+        if not authorization_header or not authorization_header.startswith('Bearer '):
+            raise AuthenticationFailed('Invalid or missing Bearer token!')
+
+        token = authorization_header.split('Bearer ')[1].strip()  # Strip whitespaces
+
+        try:
+            # Make sure to use the same secret key that was used to encode the JWT
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('JWT has expired!')
+        except jwt.DecodeError:
+            raise AuthenticationFailed('JWT is invalid!')
+
+        user = Post.objects.filter(id=payload['id']).first()
+
+        if not user:
+            raise AuthenticationFailed('User not found!')
+
+        profile = Post.objects.filter(user=user).first()
+
+        if not profile:
+            raise AuthenticationFailed('Profile not found!')
+
+        # You should import and use your profile serializer here
+        serializer =  PostSerializer(profile)
+
+        return Response(serializer.data)      
+
+
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    def get(self, request):
+        authorization_header = request.headers.get('Authorization')
+        if not authorization_header or not authorization_header.startswith('Bearer '):
+            raise AuthenticationFailed('Invalid or missing Bearer token!')
+
+        token = authorization_header.split('Bearer ')[1].strip()  # Strip whitespaces
+
+        try:
+            # Make sure to use the same secret key that was used to encode the JWT
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('JWT has expired!')
+        except jwt.DecodeError:
+            raise AuthenticationFailed('JWT is invalid!')
+
+        user = Post.objects.filter(id=payload['id']).first()
+
+        if not user:
+            raise AuthenticationFailed('User not found!')
+
+        profile = Post.objects.filter(user=user).first()
+
+        if not profile:
+            raise AuthenticationFailed('Profile not found!')
+
+        # You should import and use your profile serializer here
+        serializer =  PostSerializer(profile)
+
+        return Response(serializer.data)      
+
+
 
 class CommentListCreateView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
-    
-    # permission_classes = [IsAuthenticated]
-
     queryset = Comment.objects.all()
-    # def get_queryset(self):
-    #     post_id = self.kwargs['pk']
-    #     return Comment.objects.filter(post_id=post_id)
-    # def perform_create(self, serializer):
-    #     post_id = self.kwargs.get('post_id')
-    #     post = get_object_or_404(Post, pk=post_id)
-    #     serializer.save(author=self.request.user, post=post)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def get(self, request):
+        authorization_header = request.headers.get('Authorization')
+        if not authorization_header or not authorization_header.startswith('Bearer '):
+            raise AuthenticationFailed('Invalid or missing Bearer token!')
+
+        token = authorization_header.split('Bearer ')[1].strip()  # Strip whitespaces
+
+        try:
+            # Make sure to use the same secret key that was used to encode the JWT
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('JWT has expired!')
+        except jwt.DecodeError:
+            raise AuthenticationFailed('JWT is invalid!')
+
+        user = Comment.objects.filter(id=payload['id']).first()
+
+        if not user:
+            raise AuthenticationFailed('User not found!')
+
+        profile = Comment.objects.filter(user=user).first()
+
+        if not profile:
+            raise AuthenticationFailed('Profile not found!')
+
+        # You should import and use your profile serializer here
+        serializer =  CommentSerializer(profile)
+
+        return Response(serializer.data)      
+
+
+
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def get(self, request):
+        authorization_header = request.headers.get('Authorization')
+        if not authorization_header or not authorization_header.startswith('Bearer '):
+            raise AuthenticationFailed('Invalid or missing Bearer token!')
+
+        token = authorization_header.split('Bearer ')[1].strip()  # Strip whitespaces
+
+        try:
+            # Make sure to use the same secret key that was used to encode the JWT
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('JWT has expired!')
+        except jwt.DecodeError:
+            raise AuthenticationFailed('JWT is invalid!')
+
+        user = Comment.objects.filter(id=payload['id']).first()
+
+        if not user:
+            raise AuthenticationFailed('User not found!')
+
+        profile = Comment.objects.filter(user=user).first()
+
+        if not profile:
+            raise AuthenticationFailed('Profile not found!')
+
+        # You should import and use your profile serializer here
+        serializer =  CommentSerializer(profile)
+
+        return Response(serializer.data)      
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/alamin/Desktop/last check/probasi-dasboard-api/venv"
+setenv VIRTUAL_ENV "/home/alamin/Desktop/Intelligentdive/probasi-dasboard-api/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
